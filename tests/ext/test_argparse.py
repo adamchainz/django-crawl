5dd1f167e1f0183d8e04a0f7724332c164e4b3a5
Adam Johnson <me@adamj.eu>
2026-07-09 23:02:54 +0100

    Use unittest-parametrize for parametrized tests

--- tests/ext/test_argparse.py
+++ tests/ext/test_argparse.py
@@ -2,14 +2,16 @@ from __future__ import annotations
 
 from argparse import ArgumentTypeError
 
-from django.test import TestCase
+from django.test import SimpleTestCase
+from unittest_parametrize import ParametrizedTestCase, parametrize
 
 from django_crawl.ext import argparse as ext_argparse
 
 
-class ArgparseTests(TestCase):
-    def test_int_argument_parsers_reject_invalid_values(self):
-        cases = [
+class ArgparseTests(ParametrizedTestCase, SimpleTestCase):
+    @parametrize(
+        ("function", "value", "message"),
+        [
             (ext_argparse.non_negative_int, "x", "must be an integer"),
             (
                 ext_argparse.non_negative_int,
@@ -20,11 +22,11 @@ class ArgparseTests(TestCase):
             (ext_argparse.positive_int, "0", "must be greater than 0"),
             (ext_argparse.max_query_variants, "x", "must be an integer"),
             (ext_argparse.max_query_variants, "0", "must be greater than 0"),
-        ]
-        for function, value, message in cases:
-            with self.subTest(function=function, value=value):
-                with self.assertRaisesRegex(ArgumentTypeError, message):
-                    function(value)
+        ],
+    )
+    def test_int_argument_parsers_reject_invalid_values(self, function, value, message):
+        with self.assertRaisesRegex(ArgumentTypeError, message):
+            function(value)
 
     def test_positive_int_accepts_positive_values(self):
         assert ext_argparse.positive_int("1") == 1
